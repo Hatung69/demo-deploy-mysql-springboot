09096a5e908c7b9b57015666bd2398ed8c75e07f
hatung69 <keodeo10@gmail.com>
2021-06-19 11:18:21 +0700

    Delete student function

--- src/main/java/com/deploy/RestControllerZZ.java
+++ src/main/java/com/deploy/RestControllerZZ.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ import com.deploy.enities.Student;
 import com.deploy.repo.StudentRepo;
 
 @RestController
-@CrossOrigin(origins="*")// in this line add your url and thats is all for spring boot side
+@CrossOrigin(origins = "*") // in this line add your url and thats is all for spring boot side
 public class RestControllerZZ {
 	static final Logger log = Logger.getLogger(RestControllerZZ.class);
 
@@ -39,10 +40,17 @@ public class RestControllerZZ {
 		return ResponseEntity.ok(students);
 	}
 
-	// Get list Student
+	// Add Student
 	@PostMapping("api/v1/students")
 	public ResponseEntity<Student> addStudent(@RequestBody Student student) {
 		studentRepo.save(student);
 		return new ResponseEntity<Student>(student, HttpStatus.CREATED);
+	}
+
+	// Delete Student
+	@PostMapping("api/v1/students/id")
+	public ResponseEntity<Student> deleteStudent(@PathVariable(name = "id") int id) {
+		studentRepo.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
