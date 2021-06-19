1ec4538f6b156f9d89bdb9f72774395f409a0f8c
hatung69 <keodeo10@gmail.com>
2021-06-19 11:08:42 +0700

    Fix CROSS

--- src/main/java/com/deploy/RestControllerZZ.java
+++ src/main/java/com/deploy/RestControllerZZ.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import com.deploy.enities.Student;
 import com.deploy.repo.StudentRepo;
 
 @RestController
+@CrossOrigin(origins="*")// in this line add your url and thats is all for spring boot side
 public class RestControllerZZ {
 	static final Logger log = Logger.getLogger(RestControllerZZ.class);
 
