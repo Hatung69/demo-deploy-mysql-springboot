08cd5e4efb08e877318e9a44b3e21244cd9bfbad
hatung69 <keodeo10@gmail.com>
2021-06-19 11:19:22 +0700

    fix mapping method

--- src/main/java/com/deploy/RestControllerZZ.java
+++ src/main/java/com/deploy/RestControllerZZ.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,7 +49,7 @@ public class RestControllerZZ {
 	}
 
 	// Delete Student
-	@PostMapping("api/v1/students/id")
+	@DeleteMapping("api/v1/students/id")
 	public ResponseEntity<Student> deleteStudent(@PathVariable(name = "id") int id) {
 		studentRepo.deleteById(id);
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
